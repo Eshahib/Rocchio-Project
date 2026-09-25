729f65ca41ec3469c33a315fac483cf5a2664004
Eshahib <eshahib@hotmail.com>
2026-09-24 20:28:20 -0400

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/Rocchio-Project/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
